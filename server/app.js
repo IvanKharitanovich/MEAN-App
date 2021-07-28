0b04dcd68a0dd93d1342693daaded95f1fc9ba77
ikharytanovich <ikharytanovich@elinext.com>
2021-07-28 18:01:34 +0300

    commit message

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require(`express`);
+const analyticsRoute = require(`./routes/analytics`);
 const authRoute = require(`./routes/auth`);
+const categoryRoute = require(`./routes/category`);
+const orderRoute = require(`./routes/order`);
+const positionRoute = require(`./routes/position`);
 const app = express();
 
-app.use('/api/', authRoute);
+app.use('/api/analytics', analyticsRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/category', categoryRoute);
+app.use('/api/order', orderRoute);
+app.use('/api/position', positionRoute);
 
 module.exports = app;
