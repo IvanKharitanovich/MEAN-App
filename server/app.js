480283422ee0963d6725ad7216d8eab67c325269
ikharytanovich <ikharytanovich@elinext.com>
2021-07-29 11:31:22 +0300

    commit message

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 const express = require(`express`);
+const mongoose = require(`mongoose`);
+const bodyParser = require(`body-parser`);
+const cors = require(`cors`);
+const morgan = require(`morgan`);
+const keys = require(`./config/keys`)
 const analyticsRoute = require(`./routes/analytics`);
 const authRoute = require(`./routes/auth`);
 const categoryRoute = require(`./routes/category`);
 const orderRoute = require(`./routes/order`);
 const positionRoute = require(`./routes/position`);
 const app = express();
+
+mongoose.connect(`${keys.mongoURI}`).then(() => {
+        console.log(`CONNECTED TO DB`)
+    }
+)
+
+app.use(morgan(`dev`));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/analytics', analyticsRoute);
 app.use('/api/auth', authRoute);
@@ -13,3 +28,5 @@ app.use('/api/order', orderRoute);
 app.use('/api/position', positionRoute);
 
 module.exports = app;
+
+
---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb+srv://devuser:TA2tBzRPS8eu5F4U@cluster0.ei57y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
