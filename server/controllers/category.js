9e60386877777eae8e12c698b4e74ad2d9d461fc
ikharytanovich <ikharytanovich@elinext.com>
2021-07-29 14:56:00 +0300

    commit message

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,21 +1,37 @@
+const Category = require(`../models/Category`);
+const errorHandler = require(`../utils/errorHandler`);
+
 module.exports.getAll = function (req, res) {
-    res.status(200).json({
-        message: `CORRECT`
-    })
 }
 
 module.exports.getById = function (req, res) {
+    try {
 
+    } catch (error) {
+
+    }
 }
 
 module.exports.remove = function (req, res) {
+    try {
 
+    } catch (error) {
+
+    }
 }
 
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (error) {
+
+    }
 }
 
 module.exports.update = function (req, res) {
+    try {
 
+    } catch (error) {
+
+    }
 }
